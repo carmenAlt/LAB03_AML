dae7d6510f55d7f3a97cb26957c4e7c394592e8f
carmenAlt <it+carmenQueen@politorocketteam.it>
2025-11-04 21:16:31 +0100

    fix model

--- models/model.py
+++ models/model.py
@@ -1,9 +1,8 @@
 import torch
 from torch import nn
-
 class CustomNet(nn.Module):
     def __init__(self):
-        super(CustomNet, self).__init__()
+        super().__init__()  # <--- versione Python 3
         # Convolutional layers
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
         self.pool1 = nn.MaxPool2d(2, 2)  # 64 -> 32
@@ -14,14 +13,13 @@ class CustomNet(nn.Module):
         self.conv3 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
         self.pool3 = nn.MaxPool2d(2, 2)  # 16 -> 8
 
-        # Fully connected layer
-        self.fc1 = nn.Linear(256 * 8 * 8, 200)  # 200 classi Tiny-ImageNet
+        self.fc1 = nn.Linear(256 * 8 * 8, 200)
 
     def forward(self, x):
         x = self.pool1(self.conv1(x).relu())
         x = self.pool2(self.conv2(x).relu())
         x = self.pool3(self.conv3(x).relu())
-
         x = x.flatten(start_dim=1)
         x = self.fc1(x)
         return x
+
